65dab1f86859a6c3f01becb3c326cf7f844d0aa8
YatoNoragami1 <maximus17345@gmail.com>
2023-09-30 19:16:41 +0200

    add README.md, add storygraph-time.js, add manifest.json, NOT SIGNED YET

---
+++ storygraph-time.js
@@ -0,0 +1,89 @@
+const WPM = 350;
+let currentUrl = window.location.href;
+
+function addEstimateHoursBooks(){
+    let contentToRead = document.getElementsByClassName("col-span-5");
+    for(let i = 0; i < contentToRead.length; i++){
+        if(contentToRead[i].childNodes[3]){
+            text = contentToRead[i].childNodes[3].textContent;
+            if (!text.includes("h")){
+                matches = text.match(/\d+/);
+                if(matches){
+                    const pageCount = parseInt(matches[0]);
+                    let EstimateHours = (((pageCount*300)/WPM)/60).toFixed(1);
+                    contentToRead[i].childNodes[3].textContent += EstimateHours + "h";
+                }
+            }
+            
+
+        }
+    }
+}
+
+function addEstimateHoursBook(){
+    let contentToRead = document.getElementsByClassName("col-span-2");
+    if(contentToRead[0].childNodes[3]){
+        text = contentToRead[0].childNodes[3].textContent;
+        if (!text.includes("h")){
+            matches = text.match(/\d+/);
+            if(matches){
+                const pageCount = parseInt(matches[0]);
+                let EstimateHours = (((pageCount*300)/WPM)/60).toFixed(1);
+                contentToRead[0].childNodes[3].textContent += EstimateHours + "h";
+            }
+        }
+        
+    }
+
+}
+
+function timeSpentReading(){
+    let contentToRead = document.getElementsByClassName("my-2");
+    text = contentToRead[0].childNodes[3].textContent
+    matches = text.match(/\d+/g);
+    let pagesRead = 0;
+    if(matches){
+        for(let i = 1; i < matches.length; i++){
+            pagesRead += matches[i];
+        }
+
+        pagesRead = parseInt(pagesRead)
+        hoursRead = ((pagesRead*300)/WPM)/60;
+        daysRead = (hoursRead/24).toFixed(1)
+        contentToRead[0].childNodes[3].textContent += ", " + ((hoursRead).toFixed(0)) + " hours" + " / " + daysRead + " days";
+    }
+}
+
+if (currentUrl == "https://app.thestorygraph.com/recommendations"){
+    setTimeout(addEstimateHoursBooks, 3000);
+}
+else if (currentUrl.startsWith("https://app.thestorygraph.com/books/")){
+    setTimeout(addEstimateHoursBook, 300);
+}
+else if (currentUrl.startsWith("https://app.thestorygraph.com/stats/")){
+    setTimeout(timeSpentReading, 500);
+}
+else{
+    setTimeout(addEstimateHoursBooks, 800);
+}
+setInterval(function() {
+    
+    if (window.location.href !== currentUrl) {
+        currentUrl = window.location.href
+        console.log(currentUrl)
+        if (currentUrl == "https://app.thestorygraph.com/recommendations"){
+            setTimeout(addEstimateHoursBooks, 3000);
+        }
+        else if (currentUrl.startsWith("https://app.thestorygraph.com/books/")){
+            setTimeout(addEstimateHoursBook, 300);
+        }
+        else if (currentUrl.startsWith("https://app.thestorygraph.com/stats/")){
+            setTimeout(timeSpentReading, 300);
+        }
+        else{
+            setTimeout(addEstimateHoursBooks, 800);
+        }
+    }
+}, 1000); // Check every 1 second (adjust as needed)
+
+
