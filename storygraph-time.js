cc8304fa87937479485f8476416700f3203f9dd1
YatoNoragami1 <maximus17345@gmail.com>
2023-09-30 20:55:44 +0200

    calculate Estimated Reading Time on TBR in stats screen

--- storygraph-time.js
+++ storygraph-time.js
@@ -39,26 +39,54 @@ function addEstimateHoursBook(){
 
 function timeSpentReading(){
     let contentToRead = document.getElementsByClassName("my-2");
-    text = contentToRead[0].childNodes[3].textContent
-    matches = text.match(/\d+/g);
-    let pagesRead = 0;
-    if(matches){
-        for(let i = 1; i < matches.length; i++){
-            pagesRead += matches[i];
-        }
+    let text = contentToRead[0].childNodes[3].textContent
+    if (!text.includes("hours")){
+        let matches = text.match(/\d+/g);
+        let pagesRead = 0;
+        if(matches){
+            for(let i = 1; i < matches.length; i++){
+                pagesRead += matches[i];
+            }
 
-        pagesRead = parseInt(pagesRead)
-        hoursRead = ((pagesRead*300)/WPM)/60;
-        daysRead = (hoursRead/24).toFixed(1)
-        contentToRead[0].childNodes[3].textContent += ", " + ((hoursRead).toFixed(0)) + " hours" + " / " + daysRead + " days";
+            pagesRead = parseInt(pagesRead)
+            hoursRead = ((pagesRead*300)/WPM)/60;
+            daysRead = (hoursRead/24).toFixed(1)
+            contentToRead[0].childNodes[3].textContent += ", " + ((hoursRead).toFixed(0)) + " hours" + " / " + daysRead + " days";
+        }
     }
 }
 
+function timeToBeRead(){
+    let documentFormatDiv = document.getElementsByClassName("dark:block")
+    let contentToRead = document.getElementsByClassName("my-2");
+    let contentText = contentToRead[0].childNodes[3].textContent;
+    let text = documentFormatDiv[7].childNodes[3].textContent
+    console.log(text)
+    if (!contentText.includes("hours")){
+        let matches = text.match(/\d+/g)
+        let pagesRead = 0;
+        if(matches){
+        for(let i = 0; i < matches.length; i++){
+            pagesRead += matches[i];
+        }
+        pagesRead = parseInt(pagesRead);
+        console.log(pagesRead)
+        hoursRead = ((pagesRead*300)/WPM)/60;
+        daysRead = (hoursRead/24).toFixed(1)
+        contentToRead[0].childNodes[3].textContent += ", " + ((hoursRead).toFixed(0)) + " hours" + " / " + daysRead + " days";
+        }
+    }
+}
+
+console.log(currentUrl)
 if (currentUrl == "https://app.thestorygraph.com/recommendations"){
     setTimeout(addEstimateHoursBooks, 3000);
 }
 else if (currentUrl.startsWith("https://app.thestorygraph.com/books/")){
     setTimeout(addEstimateHoursBook, 300);
+
+}else if (currentUrl.includes("stats_type=To-Read" )){
+    setTimeout(timeToBeRead, 500);
 }
 else if (currentUrl.startsWith("https://app.thestorygraph.com/stats/")){
     setTimeout(timeSpentReading, 500);
@@ -77,6 +105,12 @@ setInterval(function() {
         else if (currentUrl.startsWith("https://app.thestorygraph.com/books/")){
             setTimeout(addEstimateHoursBook, 300);
         }
+        else if (currentUrl.includes("stats_type=To-Read" )){
+            setTimeout(timeToBeRead, 500);
+        }
+        else if (currentUrl.startsWith("https://app.thestorygraph.com/stats/")){
+            setTimeout(timeSpentReading, 500);
+        }
         else if (currentUrl.startsWith("https://app.thestorygraph.com/stats/")){
             setTimeout(timeSpentReading, 300);
         }
@@ -84,6 +118,6 @@ setInterval(function() {
             setTimeout(addEstimateHoursBooks, 800);
         }
     }
-}, 1000); // Check every 1 second (adjust as needed)
+}, 1000); 
 
 
